b01053c54ad23900be51fda28cb491a94b08e255
Xavier Herman <xavier.r.herman@gmail.com>
2023-11-02 09:48:08 -0500

    Change config

--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,15 @@ const config = {
 
  kit: {
  // hydrate the <div id="svelte"> element in src/app.html
-  adapter: adapter({
+	adapter: adapter({
 		strict: false,
-    pages: 'build',
-    assets: 'build',
-    fallback: null
-  })
+		pages: 'build',
+		assets: 'build',
+		fallback: null
+	}),
+	paths: {
+		base: '/BeyondWordsProj'
+	}
  }
 };
 
